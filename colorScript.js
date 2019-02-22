51f88ea5ee14adb2ce1af05c8f25d953745d0b66
Shaer Hasan <shaer2m@gmail.com>
2019-02-22 17:56:17 -0500

    Added reset,easy,hard buttons

--- colorScript.js
+++ colorScript.js
@@ -1,14 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-var pickedColor = colors[4];
+var colors = generateRandomColors(6);
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var pickedColor = colors[Math.floor(Math.random()*colors.length)];
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
@@ -27,6 +22,8 @@ squares.forEach(function (element, i) {
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "You Got It"
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?"
         } else {
              this.style.backgroundColor = "black"
             messageDisplay.textContent = "Try Again"
@@ -40,3 +37,34 @@ function changeColors(color) {
         squares[i].style.backgroundColor = color;
     }
 }
+
+function generateRandomColors(num) {
+    var arr = [];
+
+    for( var i = 0; i< num; i++){
+        //get random colors
+        arr.push(randomColor());
+
+    }
+
+    return arr;
+}
+
+function randomColor() {
+    // pick a red, green, blue from 0 - 255
+    var r = Math.ceil(Math.random()*255);
+    var g = Math.ceil(Math.random()*255);
+    var b = Math.ceil(Math.random()*255);
+    //rgb(0, 0 ,255) form
+    var rgb = "rgb(" + r +", " + g+", "+b+")";
+    return rgb;
+}
+resetButton.addEventListener("click", function () {
+    colors = generateRandomColors(6);
+    pickedColor = colors[Math.floor(Math.random()*colors.length)];
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i< squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "darkgray";
+});
